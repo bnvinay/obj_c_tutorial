dbb21ce4de704ea7e54fe75d63b7e012d1f059b8
User <user@Admins-Mac-mini.local>
2012-11-09 11:19:47 +0530

    "variables"

---
+++ variables/variables/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  variables
+//
+//  Created by User on 09/11/12.
+//  Copyright (c) 2012 User. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        int n1 = 43;
+        int n2 = 22;
+        int sum = n1 + n2;
+        
+        NSLog(@"the sum of %i and %i is %i", n1, n2, sum);
+        
+    }
+    return 0;
+}
+
