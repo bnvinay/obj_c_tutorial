6d116ed0bc7324a02ad00e4f8c20aa0a7b1273ab
Ganesh <dev@Admins-Mac-mini-2.local>
2012-11-09 15:07:02 +0530

    "Introduction to for looping"

---
+++ Intro_to_looping/Intro_to_looping/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  Intro_to_looping
+//
+//  Created by Ganesh on 09/11/12.
+//  Copyright (c) 2012 USER. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        int x;
+        // for loop to print x value from 1 to 10
+        for(x=1;x<=10;x++)
+            NSLog(@"%i",x);
+    }
+    return 0;
+}
+
