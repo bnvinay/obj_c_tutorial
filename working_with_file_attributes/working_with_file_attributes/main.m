3e6b44b343eccdf68670cafc3861b5482fd7ef9d
Vinay <user@Admins-Mac-mini.local>
2012-11-15 16:08:01 +0530

    "working with file attributes"

---
+++ working_with_file_attributes/working_with_file_attributes/main.m
@@ -0,0 +1,34 @@
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSString *testerfile = @"/Users/user/Desktop/testfile";
+        NSFileManager *manager;
+        
+        //create a file manager
+        manager = [NSFileManager defaultManager];
+        
+        //make sure that the file exists
+        if([manager fileExistsAtPath:testerfile]==NO){
+            NSLog(@"File does not exist!");
+            return 1;
+        }
+        
+        //get size of newfile2
+        NSDictionary *dic;
+        if((dic=[manager attributesOfItemAtPath:@"/Users/user/Desktop/testfile" error:NULL])==nil){
+            NSLog(@"couldnt get the file attributes!");
+            return 4;
+        }else{
+            NSLog(@"File is %i bytes", [[dic objectForKey:NSFileSize]intValue]);
+        }
+
+        
+    }
+    return 0;
+}
+
